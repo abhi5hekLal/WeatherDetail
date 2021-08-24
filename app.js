abe02ec4b451696c0bedbe4222bbcf48a66ad5f0
Abhishek Lal <abhisheklalsxs@gmail.com>
2021-08-24 21:14:59 +0530

    Prepare for deployment

--- app.js
+++ app.js
@@ -26,8 +26,9 @@ app.post('/', function(req, res) {
                 res.send();
             });
         });
-})
-app.listen(3000, function() {
+});
+
+app.listen(process.env.PORT ||3000, function() {
     console.log('Server running on port 3000');
 });
 
